37b2f0a47ce5fbe561f10ddba258030a2d02fb14
Petrik Lajos Szg <info@petrik.hu>
2020-09-23 13:13:07 +0200

    finish

--- repo/Program.cs
+++ repo/Program.cs
@@ -13,13 +13,25 @@ namespace repo
             int szam = 0;
             Console.WriteLine("hány szmotszeretne megadin?");
             int n = Convert.ToInt32(Console.ReadLine());
+            int min = int.MaxValue;
+            int max = int.MinValue;
             for (int i = 0; i < n; i++)
             {
                 Console.WriteLine("adjon meg az {0} számot", i);
                 szam = Convert.ToInt32(Console.ReadLine());
+                if (szam < min)
+                {
+                    min = szam;
+                }
+                if (szam > max)
+                {
+                    max = szam;
+                }
 
-                Console.WriteLine("a számok átlaga: "+(szam / 10));
             }
+
+
+            Console.WriteLine("a számok átlaga: "+(szam / 10));
         }
     }
 }
